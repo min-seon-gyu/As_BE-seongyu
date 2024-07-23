2083bb6a56e4bf81420ddc3a800d718d9aa877be
hyundoo <hyundoo1006@gmail.com>
2024-07-23 23:12:12 +0900

    feat: 엔티티 수정에 따른 dto 수정

--- src/main/java/Auction_shop/auction/product/dto/ProductDto.java
+++ src/main/java/Auction_shop/auction/product/dto/ProductDto.java
@@ -12,20 +12,28 @@ public class ProductDto {
         private String trade;           // 거래 방식
         private int initial_price;       // 시작 가격
 
-        private String details;         // 설명
+        private int current_price;      // 현재 가격
 
-        private LocalDateTime start_at; // 시작 날짜
-        private LocalDateTime end_at;   // 종료 날짜
+        private int minimum_price;      // 최저 가격
+
+        private LocalDateTime startTime; // 경매 시작 시간
+        private LocalDateTime endTime;   // 경매 종료 시간
+
+        private String details;         // 설명
 
         @Builder
         public ProductDto(Long seller, String title, String product_type, String trade, int initial_price,
-                          String details, String image_url, LocalDateTime start_at, LocalDateTime end_at) {
+                          String details, int minimum_price, LocalDateTime startTime, LocalDateTime endTime) {
                 this.seller = seller;
                 this.title = title;
                 this.product_type = product_type;
                 this.trade = trade;
                 this.initial_price = initial_price;
-                this.start_at = start_at;
-                this.end_at = end_at;
+                this.current_price = initial_price;
+                this.minimum_price = minimum_price;
+                this.startTime = startTime;
+                this.endTime = endTime;
+                this.details = details;
+
         }
 }
