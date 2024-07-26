3c35fd93555d77aff24ab2d07f2e6606d4f98b88
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2024-07-26 15:43:49 +0900

    feat: 문의 등록 시 member 객체 저장 및 기능 추가
    특정 멤버의 문의 조회 기능 추가

--- src/main/java/Auction_shop/auction/domain/inquriy/service/InquiryService.java
+++ src/main/java/Auction_shop/auction/domain/inquriy/service/InquiryService.java
@@ -4,8 +4,10 @@ import Auction_shop.auction.domain.image.Image;
 import Auction_shop.auction.domain.image.service.ImageService;
 import Auction_shop.auction.domain.inquriy.Inquiry;
 import Auction_shop.auction.domain.inquriy.repository.InquiryRepository;
-import Auction_shop.auction.web.dto.InquiryCreateDto;
-import Auction_shop.auction.web.dto.InquiryUpdateDto;
+import Auction_shop.auction.domain.member.Member;
+import Auction_shop.auction.domain.member.service.MemberService;
+import Auction_shop.auction.web.dto.inquiry.InquiryCreateDto;
+import Auction_shop.auction.web.dto.inquiry.InquiryUpdateDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,20 +23,21 @@ public class InquiryService {
 
     private final InquiryRepository inquiryRepository;
     private final ImageService imageService;
+    private final MemberService memberService;
 
     //문의 등록
     //User 관련 주석처리
     @Transactional
-    public Inquiry createInquiry(InquiryCreateDto inquiryDto, List<MultipartFile> images){
-//        Long memberId = inquiryDto.getId();
-//        Member member = memberRepository.findById(memberId)
-//                .orElseThrow(() -> new EntityNotFoundException(memberId + "에 해당하는 유저가 없습니다."));
+    public Inquiry createInquiry(InquiryCreateDto inquiryDto, Long memberId, List<MultipartFile> images){
+        Member member = memberService.getById(memberId);
         Inquiry inquiry = Inquiry.builder()
-//                .member(member);
+                .member(member)
                 .title(inquiryDto.getTitle())
                 .content(inquiryDto.getContent())
                 .status(false)
                 .build();
+
+        member.addInquiry(inquiry);
 
         List<Image> imageList = imageService.saveImages(images);
         inquiry.setImageList(imageList);
@@ -42,7 +45,7 @@ public class InquiryService {
         return inquiryRepository.save(inquiry);
     }
 
-    //문의 전체 조회
+    //문의 전체 조회 (Admin 전용)
     public List<Inquiry> getAllInquiry() {
         List<Inquiry> all = inquiryRepository.findAll();
         return inquiryRepository.findAll();
@@ -56,11 +59,10 @@ public class InquiryService {
     }
 
     //유저 문의 조회
-//    public List<Inquiry> getAllByMemberId(){
-//        Member member = memberRepository.findById(memberId)
-//                .orElseThrow(() -> new EntityNotFoundException(memberId + "에 해당하는 유저가 없습니다."));
-//        return inquiryRepository.findByUserId(member.getId());
-//    }
+    public List<Inquiry> getAllByMemberId(Long memberId){
+        Member member = memberService.getById(memberId);
+        return inquiryRepository.findByMemberId(member.getId());
+    }
 
     //itemId로 문의글 찾기
     //유저끼리의 채팅이 있지만 제때 확인 못 할 경우를 대비해 아이템별 문의 기능 구현 준비
