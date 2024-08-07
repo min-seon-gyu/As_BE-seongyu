44ab0326107d1a18be1ee3bd63dbf9de9426df20
Han <inte098@naver.com>
2024-08-07 18:35:40 +0900

    fix: appspec.yml 수정 secrets 환경 변수 가져옴

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,18 @@ APP_NAME=AuctionApp
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
+# 환경 변수를 설정합니다.
+export MYSQL_URL={{MYSQL_URL}}
+export MYSQL_USERNAME={{MYSQL_USERNAME}}
+export MYSQL_PASSWORD={{MYSQL_PASSWORD}}
+export REDIS_HOST={{REDIS_HOST}}
+export REDIS_PORT={{REDIS_PORT}}
+export MONGODB_HOST={{MONGODB_HOST}}
+export MONGODB_PORT={{MONGODB_PORT}}
+export JWT_SECRET={{JWT_SECRET}}
+export AWS_ACCESS_KEY={{AWS_ACCESS_KEY}}
+export AWS_SECRET_KEY={{AWS_SECRET_KEY}}
+
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 CURRENT_PID=$(pgrep -f $APP_NAME)
 echo "$CURRENT_PID"
