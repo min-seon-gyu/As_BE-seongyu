294018a016d36d0258960169b2e1ffe31dcbe51f
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2024-08-11 01:32:52 +0900

    feat: AuditorAware Bean 추가

--- src/main/java/Auction_shop/auction/AuctionApplication.java
+++ src/main/java/Auction_shop/auction/AuctionApplication.java
@@ -1,7 +1,10 @@
 package Auction_shop.auction;
 
+import Auction_shop.auction.util.AuditorAwareImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -9,6 +12,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 public class AuctionApplication {
+
+	@Bean
+	public AuditorAware<String> auditorAware() {
+		return new AuditorAwareImpl();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuctionApplication.class, args);
