0ac4be4fc9bb29baf654c7dabfda044b60fba797
Han <inte098@naver.com>
2024-08-12 02:54:24 +0900

    comment: 채팅 주석 추가 컨트롤러단에서 Stomp 프로토콜의 publish/subscribe를 통해 메세지를 전달하는 과정 작성

--- src/main/java/Auction_shop/auction/chat/controller/ChatController.java
+++ src/main/java/Auction_shop/auction/chat/controller/ChatController.java
@@ -23,10 +23,15 @@ public class ChatController {
 
     @MessageMapping("/chat/{roomId}")
     public void processMessage(@DestinationVariable String roomId, Chat chat) throws Exception {
-        String destination = "/sub/roomId/" + roomId;
+        // NOTE Stomp는 메세지 전송을 위한 프로토콜로 pub - sub(발행 - 구독)을 통해 동작합니다
+        // NOTE 웹소켓 연결 후 클라이언트에서 서버로 STOMP 연결 요청을 하고 성공 시 >>>CONNECTED\n id:~~\n destination:~~ 처럼 전달(frame 구조)
+        // NOTE 이후 사용자1,사용자2가 특정 경로를 구독합니다. /sub/room/방번호3 (/sub/room 경로는 고정입니다)
+        // NOTE 사용자3이 /pub/chat/방번호3의 경로로 메세지를 보내면 방번호3을 구독한 사용자1, 사용자2에게 메세지가 전달됩니다.
+
+        String destination = "/sub/roomId/" + roomId;   // 도착 경로 지정
         chatService.createChat(chat);
         try {
-            messagingTemplate.convertAndSend(destination, chat.get메세지());
+            messagingTemplate.convertAndSend(destination, chat.get메세지());   // 도착 경로로 메세지 전달
         } catch (Exception e) {
             log.error("ChatError = {}", e);
             messagingTemplate.convertAndSend(destination, "Error:" + e.getMessage());
