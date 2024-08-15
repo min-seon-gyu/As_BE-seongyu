6643ee7f51e19a0d910e3807f6fcf67715035d5c
Hyundoo <114637614+HyunDooBoo@users.noreply.github.com>
2024-08-15 18:24:55 +0900

    Update TestController.java

--- src/main/java/Auction_shop/auction/domain/inquriy/controller/TestController.java
+++ src/main/java/Auction_shop/auction/domain/inquriy/controller/TestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class TestController {
+    
     @GetMapping("/")
     public String test(){
         return "<h1>testPage</h1>";
