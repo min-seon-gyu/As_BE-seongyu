1dc9d40cca53d1b00fa249c7dddb68f7318acddd
Hyundoo <114637614+HyunDooBoo@users.noreply.github.com>
2024-08-15 17:30:03 +0900

    Add files via upload

---
+++ src/main/java/Auction_shop/auction/domain/inquriy/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class TestController {
+
+    @GetMapping
+    public String test(){
+        return "<h1>testPage<h1>";
+    }
+}
