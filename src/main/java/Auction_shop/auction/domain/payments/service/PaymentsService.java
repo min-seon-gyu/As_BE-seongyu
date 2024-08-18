6847ad5173ab228b9dad912ba722851afecc99cc
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2024-08-19 01:07:13 +0900

    feat: 결제 검증 기능 구현
    아임포트를 사용해서 진행
    프론트에서 결제 수행 후 결제된 값이 db에 저장된 물건 값과 일치하지 않으면 환불 진행

---
+++ src/main/java/Auction_shop/auction/domain/payments/service/PaymentsService.java
@@ -0,0 +1,84 @@
+package Auction_shop.auction.domain.payments.service;
+
+import Auction_shop.auction.domain.member.Member;
+import Auction_shop.auction.domain.member.service.MemberService;
+import Auction_shop.auction.domain.payments.Payments;
+import Auction_shop.auction.domain.payments.repository.PaymentsRepository;
+import Auction_shop.auction.domain.product.repository.ProductRepository;
+import com.siot.IamportRestClient.IamportClient;
+import com.siot.IamportRestClient.exception.IamportResponseException;
+import com.siot.IamportRestClient.request.CancelData;
+import com.siot.IamportRestClient.response.IamportResponse;
+import com.siot.IamportRestClient.response.Payment;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentsService {
+
+    private IamportClient iamportClient;
+
+    private final PaymentsRepository paymentsRepository;
+    private final ProductRepository productRepository;
+    private final MemberService memberService;
+
+    @Value("${iamport.key}")
+    private String apiKey;
+
+    @Value("${iamport.secret}")
+    private String secretKey;
+
+    public void PaymentsService(){
+        this.iamportClient = new IamportClient(apiKey, secretKey);
+    }
+
+    public String PaymentsVerify(String impUid, Long productId, Long memberId) throws IamportResponseException, IOException {
+
+        IamportResponse<Payment> iamportResponse;
+        try {
+            iamportResponse = iamportClient.paymentByImpUid(impUid);
+        } catch (Exception e) {
+            throw new RuntimeException("결제 정보를 조회하는 데 실패: " + e.getMessage());
+        }
+
+        String merchantUid = iamportResponse.getResponse().getMerchantUid();
+
+        int paidAmount = iamportResponse.getResponse().getAmount().intValue();
+        int productPrice = productRepository.findCurrentPriceById(productId);
+
+        if (paidAmount != productPrice) {
+            cancelAllPayment(impUid);
+            return "지불한 금액 : " + paidAmount + "와 데이터베이스 금액: " + productPrice + "가 일치하지 않음.";
+        }
+
+        Member member = memberService.getById(memberId);
+
+        Payments payments = Payments.builder()
+                .paymentMethod(iamportResponse.getResponse().getPayMethod())
+                .amount(paidAmount)
+                .merchantUid(merchantUid)
+                .member(member)
+                .build();
+
+        paymentsRepository.save(payments);
+
+        return "결제가 완료되었습니다.";
+    }
+
+    private void cancelAllPayment(String impUid) {
+        try {
+            CancelData cancelData = new CancelData(impUid, true);
+            cancelData.setReason("결제 금액 불일치로 인한 취소");
+
+            iamportClient.cancelPaymentByImpUid(cancelData);
+            System.out.println("결제가 취소되었습니다: " + impUid);
+
+        } catch (Exception e) {
+            throw new RuntimeException("결제 취소 실패: " + e.getMessage());
+        }
+    }
+}
