f9435e90d84c7a64234bd4c641b81ca3b3f95ca4
Han <inte098@naver.com>
2024-08-28 14:30:38 +0900

    Fix: ChatDto id 타입 수정, MongoConfiguration 수정 Chat 테이블에 생성시간 컬럼이 포함되지 않는 문제 해결

--- src/main/java/Auction_shop/auction/chat/common/MongoConfiguration.java
+++ src/main/java/Auction_shop/auction/chat/common/MongoConfiguration.java
@@ -3,6 +3,7 @@ package Auction_shop.auction.chat.common;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.core.convert.DbRefResolver;
 import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
 import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
@@ -10,6 +11,7 @@ import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
 @Configuration
+@EnableMongoAuditing
 public class MongoConfiguration {
     /**
      * mongodb 내 _class column 제거
--- src/main/java/Auction_shop/auction/chat/dto/ChatDto.java
+++ src/main/java/Auction_shop/auction/chat/dto/ChatDto.java
@@ -3,7 +3,7 @@ package Auction_shop.auction.chat.dto;
 import java.time.LocalDateTime;
 
 public class ChatDto {
-    private Long id;
+    private String id;
     private Long roomId; // 채팅방번호
     private String 유저ID;
     private String 메세지;
