c9d1f59658662a254f78119f0864e234aae5513f
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2024-09-03 01:44:49 +0900

    feat: product es에 좋아요 갯수도 갱신

--- src/main/java/Auction_shop/auction/domain/like/service/LikeServiceImpl.java
+++ src/main/java/Auction_shop/auction/domain/like/service/LikeServiceImpl.java
@@ -5,7 +5,10 @@ import Auction_shop.auction.domain.like.repository.LikeRepository;
 import Auction_shop.auction.domain.member.Member;
 import Auction_shop.auction.domain.member.service.MemberService;
 import Auction_shop.auction.domain.product.Product;
+import Auction_shop.auction.domain.product.ProductDocument;
+import Auction_shop.auction.domain.product.elasticRepository.ProductElasticsearchRepository;
 import Auction_shop.auction.domain.product.repository.ProductJpaRepository;
+import Auction_shop.auction.web.dto.product.ProductMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +21,9 @@ public class LikeServiceImpl implements LikeService {
 
     private final LikeRepository likeRepository;
     private final ProductJpaRepository productRepository;
+    private final ProductElasticsearchRepository productElasticsearchRepository;
     private final MemberService memberService;
+    private final ProductMapper productMapper;
 
     @Override
     public Like addProductToLike(Long memberId, Long productId) {
@@ -37,6 +42,9 @@ public class LikeServiceImpl implements LikeService {
 
         member.addLike(like);
         product.addLike(like);
+
+        ProductDocument productDocument = productMapper.toDocument(product);
+        productElasticsearchRepository.save(productDocument);
 
         return likeRepository.save(like);
     }
@@ -66,6 +74,8 @@ public class LikeServiceImpl implements LikeService {
         if(like != null){
             member.removeLike(like);
             product.removeLike(like);
+            ProductDocument productDocument = productMapper.toDocument(product);
+            productElasticsearchRepository.save(productDocument);
             likeRepository.delete(like);
         }
     }
