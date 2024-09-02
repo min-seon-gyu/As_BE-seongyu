3e09ffa884610ff2e0fa749f28e9c63052955125
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2024-09-02 16:01:10 +0900

    feat: 엘라스틱서치 레포지토리 어노테이션 추가

--- src/main/java/Auction_shop/auction/AuctionApplication.java
+++ src/main/java/Auction_shop/auction/AuctionApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
 @EnableScheduling
+@EnableElasticsearchRepositories(basePackages = "Auction_shop.auction.domain.product.elasticRepository")
 @SpringBootApplication
 public class AuctionApplication {
 
