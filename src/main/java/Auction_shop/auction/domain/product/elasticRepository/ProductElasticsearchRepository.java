0f16cb38854f5f4b94b8120694b28f3418be05cd
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2024-09-10 01:40:27 +0900

    refactor: Param 사용으로 수정

--- src/main/java/Auction_shop/auction/domain/product/elasticRepository/ProductElasticsearchRepository.java
+++ src/main/java/Auction_shop/auction/domain/product/elasticRepository/ProductElasticsearchRepository.java
@@ -3,18 +3,24 @@ package Auction_shop.auction.domain.product.elasticRepository;
 import Auction_shop.auction.domain.product.ProductDocument;
 import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Set;
 
 public interface ProductElasticsearchRepository extends ElasticsearchRepository<ProductDocument, Long> {
-    Iterable<ProductDocument> findByCreatedBy(String createdBy); // 닉네임으로 제품 조회
+    // 닉네임으로 제품 조회
+    Iterable<ProductDocument> findByCreatedBy(String createdBy);
+
     @Query("{\"bool\": {\"must\": [{\"match\": {\"title\": {\"query\": \"?0\", \"operator\": \"and\"}}}, {\"term\": {\"sold\": false}}]}}}")
-    Iterable<ProductDocument> findByTitleLike(String title);
+    Iterable<ProductDocument> findByTitleLike(@Param("title") String title);
+
     @Query("{\"bool\": {\"must\": [{\"terms\": {\"categories\": ?0}}, {\"term\": {\"sold\": false}}]}}}")
-    List<ProductDocument> findByCategoriesIn(Set<String> userCategories);
+    List<ProductDocument> findByCategoriesIn(@Param("userCategories") Set<String> userCategories);
+
     @Query("{\"bool\": {\"must\": [{\"term\": {\"sold\": false}}]}}")
     List<ProductDocument> findTop20ByOrderByLikeCountDesc();
+
     @Query("{\"bool\": {\"must\": [{\"term\": {\"sold\": false}}]}}")
     List<ProductDocument> findTop20ByOrderByCreatedAtDesc();
 }
