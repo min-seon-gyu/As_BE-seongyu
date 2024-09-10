81f24621e0975d8f2de7763fb1d3e93105e567d9
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2024-09-11 00:10:57 +0900

    refactor: redis 저장 시 bid 객체 String으로 변환
    조회시엔 다시 bid 객체로 변환

--- src/main/java/Auction_shop/auction/domain/bid/repository/BidRepository.java
+++ src/main/java/Auction_shop/auction/domain/bid/repository/BidRepository.java
@@ -1,26 +1,48 @@
 package Auction_shop.auction.domain.bid.repository;
 
 import Auction_shop.auction.domain.bid.Bid;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Repository
-//redis 사용
 public class BidRepository {
 
-    private RedisTemplate<String, Bid> redisTemplate;
+    private final RedisTemplate<String, String> redisTemplate;
+    private final ObjectMapper objectMapper;
 
     public void save(Bid bid) {
         String key = "bids" + bid.getProduct_id();
-        redisTemplate.opsForList().rightPush(key, bid);
+        try {
+            String jsonBid = objectMapper.writeValueAsString(bid); // Bid 객체를 JSON 문자열로 변환
+            redisTemplate.opsForList().rightPush(key, jsonBid); // JSON 문자열로 저장
+        } catch (JsonProcessingException e) {
+            e.printStackTrace(); // 예외 처리
+        }
     }
 
     public List<Bid> findBidsByProductId(Long product_id) {
         String key = "bids" + product_id;
-        return redisTemplate.opsForList().range(key, 0, -1);
+        List<String> jsonBids = redisTemplate.opsForList().range(key, 0, -1); // JSON 문자열 리스트 가져오기
+
+        Collections.reverse(jsonBids);
+
+        return jsonBids.stream()
+                .map(json -> {
+                    try {
+                        return objectMapper.readValue(json, Bid.class); // JSON 문자열을 Bid 객체로 변환
+                    } catch (JsonProcessingException e) {
+                        e.printStackTrace(); // 예외 처리
+                        return null; // 변환 실패 시 null 반환
+                    }
+                })
+                .collect(Collectors.toList());
     }
 }
