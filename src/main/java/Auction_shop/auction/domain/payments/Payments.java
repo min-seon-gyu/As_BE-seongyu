51d1f980f03e74d7eb38cfd12f0c042069bb9493
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2024-09-13 04:07:21 +0900

    feat: impUid, product 추가

--- src/main/java/Auction_shop/auction/domain/payments/Payments.java
+++ src/main/java/Auction_shop/auction/domain/payments/Payments.java
@@ -1,6 +1,8 @@
 package Auction_shop.auction.domain.payments;
 
 import Auction_shop.auction.domain.member.Member;
+import Auction_shop.auction.domain.product.Product;
+import Auction_shop.auction.util.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,7 +14,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Payments {
+public class Payments extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,8 +23,13 @@ public class Payments {
     private String paymentMethod;
     private int amount;
     private String merchantUid;
+    private String impUid;
 
     @ManyToOne
     @JoinColumn(name = "member_id")
     private Member member;
+
+    @ManyToOne // 상품과 다대일 관계
+    @JoinColumn(name = "product_id") // 결제가 연관된 상품
+    private Product product;
 }
