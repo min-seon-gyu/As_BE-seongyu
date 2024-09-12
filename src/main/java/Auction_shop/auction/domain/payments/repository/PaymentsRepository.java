6cd99402e781fc1fa877ca7f2af7701427ec4000
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2024-09-13 04:08:30 +0900

    feat: 경매 물품에서 가장 최근에 입찰한 사람 정보 가져오는 쿼리

--- src/main/java/Auction_shop/auction/domain/payments/repository/PaymentsRepository.java
+++ src/main/java/Auction_shop/auction/domain/payments/repository/PaymentsRepository.java
@@ -3,5 +3,9 @@ package Auction_shop.auction.domain.payments.repository;
 import Auction_shop.auction.domain.payments.Payments;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PaymentsRepository extends JpaRepository<Payments, Long> {
+
+    Optional<Payments> findTopByProductIdOrderByCreatedAtDesc(Long productId);
 }
