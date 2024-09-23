50c4de5816ead8c58c286159e89cba6d65e2e2a5
hyundoo <hyundoo1006@gmail.com>
2024-09-23 19:24:59 +0900

    feat: Redis bid 저장 시 memberId key도 저장
    조회 기능도 추가

--- src/main/java/Auction_shop/auction/domain/bid/repository/BidRedisRepository.java
+++ src/main/java/Auction_shop/auction/domain/bid/repository/BidRedisRepository.java
@@ -20,16 +20,34 @@ public class BidRedisRepository {
 
     public void save(Bid bid) {
         String key = "bids" + bid.getProductId();
+        String memberKey = "bids:member:" + bid.getMemberId();
         try {
             String jsonBid = objectMapper.writeValueAsString(bid); // Bid 객체를 JSON 문자열로 변환
             redisTemplate.opsForList().rightPush(key, jsonBid); // JSON 문자열로 저장
+            redisTemplate.opsForList().rightPush(memberKey, jsonBid);
         } catch (JsonProcessingException e) {
             e.printStackTrace(); // 예외 처리
         }
     }
 
-    public List<Bid> findBidsByProductId(Long product_id) {
-        String key = "bids" + product_id;
+    public List<Bid> findBidsByProductId(Long productId) {
+        String key = "bids" + productId;
+        List<String> jsonBids = redisTemplate.opsForList().range(key, 0, -1); // JSON 문자열 리스트 가져오기
+
+        return jsonBids.stream()
+                .map(json -> {
+                    try {
+                        return objectMapper.readValue(json, Bid.class); // JSON 문자열을 Bid 객체로 변환
+                    } catch (JsonProcessingException e) {
+                        e.printStackTrace(); // 예외 처리
+                        return null; // 변환 실패 시 null 반환
+                    }
+                })
+                .collect(Collectors.toList());
+    }
+
+    public List<Bid> findBidsByMemberId(Long memberId) {
+        String key = "bids:member:" + memberId;
         List<String> jsonBids = redisTemplate.opsForList().range(key, 0, -1); // JSON 문자열 리스트 가져오기
 
         return jsonBids.stream()
