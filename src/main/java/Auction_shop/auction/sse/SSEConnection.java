8ca74ac8d9bf081ca8ab10e87492342b4c118aa1
Han <inte098@naver.com>
2024-10-09 18:28:50 +0900

    feat: RedisTemplate 추가 RedisConfig 클래스에 value 값으로 Long타입을 받도록 longRedisTemplate 메소드 추가 fix: 사용자ID값 Long 타입에서 String타입으로 변환 Redis의 Key값으로 사용하기 위해 Long -> String으로 변환

--- src/main/java/Auction_shop/auction/sse/SSEConnection.java
+++ src/main/java/Auction_shop/auction/sse/SSEConnection.java
@@ -2,6 +2,7 @@ package Auction_shop.auction.sse;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +19,8 @@ import java.util.concurrent.ConcurrentHashMap;
 @AllArgsConstructor
 @RestController
 public class SSEConnection {
-    private final RedisTemplate<Long, Long> messageQueue;
+    @Qualifier("longRedisTemplate")
+    private final RedisTemplate<String, Long> messageQueue;
     private static final Map<Long, SseEmitter> emitters = new ConcurrentHashMap<>();
     private final long TIMEOUT = 5 * 60 * 1000L;
     private final long RECONNECTION_TIMEOUT = 5000L;
@@ -32,9 +34,11 @@ public class SSEConnection {
         // Emitter 생성 후 만료 시간(TIMEOUT)까지 아무 데이터도 보내지 않는 경우 503에러가 발생한다.
         // "name: connect, data: connect success"와 같은 더미데이터를 전달하여 에러 방지
         // 오프라인 상태일 때 구매자에 의해 생성된 채팅방 번호를 전달
-        List<Long> chatRoomIds = messageQueue.opsForList().range(userId, 0, -1);
+        // Redis의 key로 문자열이 들어가기에 Long타입의 userId를 String으로 변환하여 저장
+        String idTypeChange = String.valueOf(userId);
+        List<Long> chatRoomIds = messageQueue.opsForList().range(idTypeChange, 0, -1);
         sendEvent(emitter, "connect", chatRoomIds);
-        messageQueue.delete(userId);
+        messageQueue.delete(idTypeChange);
 
         // 타임아웃이 발생하면 브라우저에서 재연결 요청을 보내고 새로운 Emitter 객체를 생성하기에 기존의 Emitter를 삭제
         emitter.onTimeout(() -> {
